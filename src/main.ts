fed91dc649160f5e9644bb63dc8680a0e85b88b2
황세웅 <0cookieboy0@gmail.com>
2024-05-10 10:49:23 +0900

    init: fontawesome icom setting

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 
-const app = createApp(App)
+library.add(fas)
+library.add(far)
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(createPinia()).use(router).mount('#app')
