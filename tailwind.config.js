15928dc963002cf5aa931bf0301b004f78a07407
황세웅 <0cookieboy0@gmail.com>
2024-05-10 10:37:35 +0900

    init: tailwind setting

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      width: {
+        vw: 'calc(100vw - (100vw - 100%))',
+      },
+    },
+  },
+  plugins: [],
+}
