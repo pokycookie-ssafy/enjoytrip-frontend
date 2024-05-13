5651a02bf83f36f0834d53d9d126d91618876f14
황세웅 <0cookieboy0@gmail.com>
2024-05-13 11:57:13 +0900

    feat: caption component

--- src/components/layout/UserNavigation.vue
+++ src/components/layout/UserNavigation.vue
@@ -2,6 +2,7 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
+import Caption from '../ui/Caption.vue'
 
 const hover = ref(false)
 
@@ -24,19 +25,33 @@ const mouseoutHandler = () => {
     :data-hover="hover"
   >
     <div class="flex justify-center items-center">
-      <button
-        class="absolute flex justify-center items-center w-10 h-10 rounded-full overflow-hidden bg-gray-800 border-gray-900 hover:bg-red-600 transition-all data-[hover=false]:opacity-0 data-[hover=true]:-translate-y-14"
+      <div
+        class="absolute flex items-center group/logout transition-all data-[hover=false]:opacity-0 data-[hover=true]:-translate-y-14"
         :data-hover="hover"
       >
-        <FontAwesomeIcon icon="fa-solid fa-xmark" />
-      </button>
-      <RouterLink
-        class="duration-300 hover:bg-indigo-500 hover:border-indigo-400 cursor-pointer flex justify-center border-2 border-indigo-500 items-center transition-all w-11 h-11 shrink-0 bg-indigo-600 rounded-full shadow-md data-[hover=true]:scale-125"
-        :to="{ name: 'profile' }"
-        :data-hover="hover"
-      >
-        <FontAwesomeIcon icon="fa-solid fa-user" />
-      </RouterLink>
+        <button
+          class="flex justify-center items-center w-10 h-10 rounded-full overflow-hidden bg-gray-800 border-gray-900 hover:bg-red-600"
+        >
+          <FontAwesomeIcon icon="fa-solid fa-xmark" />
+        </button>
+        <Caption
+          text="로그아웃"
+          class="-left-20 group-hover/logout:opacity-100 opacity-0 transition-all"
+        />
+      </div>
+      <div class="flex items-center justify-center group/profile relative">
+        <RouterLink
+          class="duration-300 hover:bg-indigo-500 hover:border-indigo-400 cursor-pointer flex justify-center border-2 border-indigo-500 items-center transition-all w-11 h-11 shrink-0 bg-indigo-600 rounded-full shadow-md data-[hover=true]:scale-125"
+          :to="{ name: 'profile' }"
+          :data-hover="hover"
+        >
+          <FontAwesomeIcon icon="fa-solid fa-user" />
+        </RouterLink>
+        <Caption
+          text="프로필 보기"
+          class="-left-24 group-hover/profile:opacity-100 opacity-0 transition-all"
+        />
+      </div>
     </div>
   </div>
 </template>
---
+++ src/components/ui/Caption.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps<{
+  text: string
+}>()
+</script>
+
+<template>
+  <div class="flex items-center pointer-events-none absolute z-[995]">
+    <div class="p-2 text-xs font-thin bg-gray-800 rounded text-nowrap">
+      {{ props.text }}
+    </div>
+    <div class="tri"></div>
+  </div>
+</template>
+
+<style scoped>
+.tri {
+  width: 0;
+  height: 0;
+  border-top: 7px solid transparent;
+  border-bottom: 7px solid transparent;
+  border-left: 7px solid #1f2937;
+  border-right: 7px solid transparent;
+}
+</style>
